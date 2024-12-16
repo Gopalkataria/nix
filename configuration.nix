992c5934b8e560c466296e74cfa46c023ac85633
Gopal <gopal.kataria@research.iiit.ac.in>
2024-12-16 12:50:58 +0530

    Update configuration - 2024-12-16 12:50:58

--- configuration.nix
+++ configuration.nix
@@ -119,6 +119,29 @@
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   hardware.bluetooth.powerOnBoot = true; # enable bluetooth on boot 
 
+  #tlp 
+  services.power-profiles-daemon.enable = false ; 
+  services.tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 20;
+
+       #Optional helps save long term battery health
+       START_CHARGE_THRESH_BAT0 = 80; 
+       STOP_CHARGE_THRESH_BAT0 =  95; 
+
+      };
+};
+
   # Install firefox.
   programs.firefox.enable = true;
 
